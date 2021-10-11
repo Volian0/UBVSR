37a6d2d64ddb58e15cf6ec4f26977fc9cfa0c529
Volian0 <40314913+Volian0@users.noreply.github.com>
2021-10-11 03:08:49 +0200

    Fixed a problem with SDL main

--- UBVSR/Window.cpp
+++ UBVSR/Window.cpp
@@ -2,43 +2,45 @@
 
 namespace ubv
 {
-	Window::Window(WindowProps t_win_props)
-		: m_win_props{ std::move(t_win_props) },  m_window_ptr {
-		SDL_CreateWindow(m_win_props.title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, m_win_props.width, m_win_props.height, SDL_WINDOW_SHOWN)
-	}
+Window::Window(WindowProps t_win_props)
+	: m_win_props{std::move(t_win_props)}, m_window_ptr{SDL_CreateWindow(m_win_props.title.c_str(),
+																		 SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+																		 m_win_props.width, m_win_props.height,
+																		 SDL_WINDOW_SHOWN)}
+{
+	if (!m_window_ptr)
 	{
-		if (!m_window_ptr) {
-			throw std::runtime_error("Failed to create window");
-		}
-	}
-
-	Window::~Window()
-	{
-		SDL_DestroyWindow(m_window_ptr);
-	}
-
-	SDL_Window* Window::get_sdl_window()
-	{
-		return m_window_ptr;
-	}
-
-	const WindowProps& Window::get_win_props() const
-	{
-		return m_win_props;
-	}
-
-	std::uint16_t Window::get_win_width() const
-	{
-		return m_win_props.width;
-	}
-
-	std::uint16_t Window::get_win_height() const
-	{
-		return m_win_props.height;
-	}
-
-	const std::string& Window::get_win_title() const
-	{
-		return m_win_props.title;
+		throw std::runtime_error("Failed to create window");
 	}
 }
+
+Window::~Window() noexcept
+{
+	SDL_DestroyWindow(m_window_ptr);
+}
+
+SDL_Window *Window::get_sdl_window() noexcept
+{
+	return m_window_ptr;
+}
+
+const WindowProps &Window::get_win_props() const noexcept
+{
+	return m_win_props;
+}
+
+std::uint16_t Window::get_win_width() const noexcept
+{
+	return m_win_props.width;
+}
+
+std::uint16_t Window::get_win_height() const noexcept
+{
+	return m_win_props.height;
+}
+
+const std::string &Window::get_win_title() const noexcept
+{
+	return m_win_props.title;
+}
+} // namespace ubv
--- UBVSR/Window.hpp
+++ UBVSR/Window.hpp
@@ -2,30 +2,34 @@
 
 #include "UBVSR.hpp"
 
-namespace ubv {
-	struct WindowProps {
-		std::uint16_t width;
-		std::uint16_t height;
-		std::string title;
+namespace ubv
+{
+struct WindowProps
+{
+	std::uint16_t width;
+	std::uint16_t height;
+	std::string title;
 
-		inline WindowProps(std::uint16_t t_width = 0, std::uint16_t t_height = 0, std::string t_title = "") : width{t_width}, height{t_height}, title{std::move(t_title)} { }
-	};
-
-	class Window
+	inline WindowProps(std::uint16_t t_width = 0, std::uint16_t t_height = 0, std::string t_title = "") noexcept
+		: width{t_width}, height{t_height}, title{std::move(t_title)}
 	{
-	public:
-		Window(WindowProps t_win_props);
-		virtual ~Window();
-
-
-		SDL_Window* get_sdl_window();
-		const WindowProps& get_win_props() const;
-		std::uint16_t get_win_width() const;
-		std::uint16_t get_win_height() const;
-		const std::string& get_win_title() const;
-
-	private:
-		WindowProps m_win_props;
-		SDL_Window* m_window_ptr;
-	};
+	}
 };
+
+class Window
+{
+  public:
+	Window(WindowProps t_win_props);
+	virtual ~Window() noexcept;
+
+	SDL_Window *get_sdl_window() noexcept;
+	const WindowProps &get_win_props() const noexcept;
+	std::uint16_t get_win_width() const noexcept;
+	std::uint16_t get_win_height() const noexcept;
+	const std::string &get_win_title() const noexcept;
+
+  private:
+	WindowProps m_win_props;
+	SDL_Window *m_window_ptr;
+};
+}; // namespace ubv
